53b8a21539b55ee1f50cbd5d4a0023479c7db409
Angel Odiel Trevino Villanueva <angel.trevinov@gmail.com>
2019-09-19 23:48:43 -0500

    Adding Practice

---
+++ PartialPractice/js/main.js
@@ -0,0 +1,7 @@
+waifuForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    Main.innerHTML = Main.innerHTML + 
+        "<div style=\"display: flex;\">" +
+        "            <h2 style=\"margin: auto; color: red\">SHE IS TRASH</h2>" +
+        "        </div>"
+});
